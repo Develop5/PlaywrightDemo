2a59121644f5b940d54d3a5656c5d3492d7835c4
Develop5 <lourdesalternativa@yahoo.com>
2023-06-01 06:24:42 +0200

    hooks.ts created with AfterAll and BeforeAll

--- react-app/e2e/src/step-definitions/hooks/hooks.ts
+++ react-app/e2e/src/step-definitions/hooks/hooks.ts
@@ -1,3 +1,14 @@
 // This will be seen in the BeforeAll 
 import {BeforeAll, Before, AfterAll, After} from "@cucumber/cucumber";
 const {chromium} = require ("playwright");
+
+BeforeAll(async() => {
+    global.browser = await chromium.launch({
+        headless:false,
+    })
+});
+
+AfterAll(async() => {
+    global.browser = await chromium.close();
+})
+
