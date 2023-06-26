22148981c82e434d169e8f1cbc8b248f8f98fd28
Develop5 <lourdesalternativa@yahoo.com>
2023-06-27 00:33:03 +0200

    Provoked error with full-name-label in page

--- react-app/src/ContactItem.react.js
+++ react-app/src/ContactItem.react.js
@@ -5,7 +5,7 @@ export class ContactItem extends React.Component {
     const {item} = this.props;
     return (
       <div className="ContactItem" data-id="contact">
-        <p data-id="full-name-label">
+        <p data-id="full-name-labelX">
             Name:<strong className="item-value" data-id="name">{item.name}</strong>
         </p>
         <p data-id="gender-label">
