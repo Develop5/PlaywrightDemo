09a7ffffbeca9ca17da310dd3a3da3312278a7bd
Develop5 <lourdesalternativa@yahoo.com>
2023-06-29 05:44:24 +0200

    Removed the pause from test

--- react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
+++ react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
@@ -15,15 +15,6 @@ Then(
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
         
         await waitFor(async() => {
-
-            await page.pause()          // Introduced to see in PlayWright Inspector
-            // how the test is paused and resume it from there
-            // To try this, it is necessary to delete the line 
-            // PWDEBUG=1
-            // from common.env
-            // Playwright Inspector will pop up and you need to click on Resume
-            // Sometimes the test will fail. Randomly. Repeat it.
-
             const elementText = await page.textContent(elementIdentifier)
             return elementText?.includes(expectedElementText)
         })
