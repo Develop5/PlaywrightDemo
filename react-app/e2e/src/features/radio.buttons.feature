d9291b1f2581fed73c2295d6b69d69f53d10e766
Develop5 <lourdesalternativa@yahoo.com>
2023-07-02 12:35:45 +0200

    Negate in a contain case too

--- react-app/e2e/src/features/radio.buttons.feature
+++ react-app/e2e/src/features/radio.buttons.feature
@@ -5,16 +5,15 @@ Feature: As a user I can interact with radio buttons
         Given I am on the "home" page
         And I click the "playground" button
         And I am directed to the "playground" page
+
+        And the "female radio button label" should contain the text "Female"
+
         And the "female" radio button should be checked
-
         And the "male" radio button should not be checked
-
         And I check the "male" radio button
         And the "male" radio button should be checked
-
         And the "female" radio button should not be checked
-        
         And I check the "female" radio button
         And the "female" radio button should be checked
-
         And the "male" radio button should not be checked
+
--- react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
+++ react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
@@ -5,25 +5,25 @@ import { ScenarioWorld } from '../setup/world'
 import { waitFor } from '../../support/wait-for-behavior'
 
 Then(
-    /^the "([^"]*)" should contain the text "(.*)"$/,
-    async function(this: ScenarioWorld, elementKey: ElementKey, expectedElementText: string){
+    /^the "([^"]*)" should( not)? contain the text "(.*)"$/,
+    async function(this: ScenarioWorld, elementKey: ElementKey, negate:boolean, expectedElementText: string){
         const {
             screen: {page},
             globalConfig,
         } = this;
-        console.log(`the ${elementKey} should contain the text ${expectedElementText}`);
+        console.log(`the ${elementKey} should ${negate?'not ':''} contain the text ${expectedElementText}`);
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
         
         await waitFor(async() => {
             const elementText = await page.textContent(elementIdentifier)
-            return elementText?.includes(expectedElementText)
+            return elementText?.includes(expectedElementText) === !negate;
         })
     }
 )
 
 Then(
     /^the "([^"]*)" should equal the text "(.*)"$/,
-    async function(this: ScenarioWorld, elementKey: ElementKey, expectedElementText: string) {
+    async function(this: ScenarioWorld, elementKey: ElementKey, negate: boolean, expectedElementText: string) {
     const {
         screen: { page },
         globalConfig,
