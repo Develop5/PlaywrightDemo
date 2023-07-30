ac584c69814d4e7c073cc145eb022917b08016e9
Develop5 <lourdesalternativa@yahoo.com>
2023-07-30 03:47:27 +0200

    PUT trying to update a non-existing post

--- react-app/api_e2e/src/tests/PUT.test.ts
+++ react-app/api_e2e/src/tests/PUT.test.ts
@@ -20,3 +20,21 @@ test('update user post', async ({ request }) => {
     } ))
 })
 
+
+test('update a user post that does not exist', async ({ request }) => {
+    const response = await request.put('/posts/200', {
+        data: {
+            id: 1,
+            title: 'Existing Post',
+            body: 'This is a post',
+            userId: 1
+        }
+    })
+
+    expect(response.ok()).toBeFalsy()
+    expect(response.status()).toBe(500)
+    expect(response.statusText()).toEqual("Internal Server Error")
+    expect(await response.text()).toContain("Cannot read properties of undefined (reading 'id')")
+
+})
+
