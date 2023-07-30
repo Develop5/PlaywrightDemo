849e4aa62a14521eeedadda13becfc33148ff295
Develop5 <lourdesalternativa@yahoo.com>
2023-07-30 03:55:24 +0200

    DELETE back to normal exection

--- react-app/api_e2e/src/tests/DELETE.test.ts
+++ react-app/api_e2e/src/tests/DELETE.test.ts
@@ -4,5 +4,5 @@ test('delete user posts', async ({ request }) => {
     const response = await request.delete('/posts/1')
     expect(response.ok()).toBeTruthy()
     expect(response.status()).toBe(200)
-    expect( await response.text()).toContain("{TESTING}")
+    expect( await response.text()).toContain("{}")
 })
